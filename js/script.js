e942fe94bc4351160305142669daea7e6a3902fe
porimol roy <168266724+porimolr@users.noreply.github.com>
2024-07-04 22:02:50 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//java 
+
+function dev_html(){
+   document.getElementById('html').innerHTML='> Hypertext Markup Language.'
+
+}
+function dev_css(){
+   document.getElementById('css').innerHTML='> Cascading Style Sheets.'
+
+}
+function dev_cms(){
+   document.getElementById('cms').innerHTML='> Content management system.'
+
+}
+ 
